5afb5c68d53752e94364a5a677a5c0bfc87bd1a5
Andy Doan <andy@foundries.io>
2025-11-20 15:15:56 -0600

    auth: Add noauth provider for testing
    Signed-off-by: Andy Doan <andy@foundries.io>

---
+++ auth/noauth.go
@@ -0,0 +1,69 @@
+//go:build !disable_noauth
+
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+package auth
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/foundriesio/dg-satellite/storage"
+	"github.com/foundriesio/dg-satellite/storage/users"
+	"github.com/labstack/echo/v4"
+)
+
+type noauthProvider struct {
+	users  *users.Storage
+	scopes users.Scopes
+}
+
+func (noauthProvider) Name() string {
+	return "noauth"
+}
+
+func (p *noauthProvider) Configure(e *echo.Echo, storage *users.Storage, authConfig *storage.AuthConfig) (err error) {
+	p.users = storage
+	p.scopes, err = users.ScopesFromSlice(authConfig.NewUserDefaultScopes)
+	return
+}
+
+func (p noauthProvider) GetUser(c echo.Context) (*users.User, error) {
+	user, err := p.users.Get("noauth-fake-user")
+	if err != nil {
+		return nil, err
+	} else if user == nil {
+		slog.Info("noauth: creating noauth-fake-user user")
+		user = &users.User{
+			Username:      "noauth-fake-user",
+			AllowedScopes: p.scopes,
+		}
+		err = p.users.Create(user)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return user, nil
+}
+
+func (p noauthProvider) GetSession(c echo.Context) (*Session, error) {
+	user, err := p.GetUser(c)
+	if err != nil {
+		return nil, err
+	} else if user == nil {
+		return nil, nil
+	}
+	return &Session{
+		BaseUrl: c.Scheme() + "://" + c.Request().Host,
+		User:    user,
+		Client:  http.DefaultClient,
+	}, nil
+}
+
+func (noauthProvider) DropSession(echo.Context, *Session) {
+}
+
+func init() {
+	RegisterProvider(&noauthProvider{})
+}
